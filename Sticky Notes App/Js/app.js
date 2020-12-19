385550e165f1dc4f647cf36f3d6b38bd649295fd
talha faisal <57016608+talha442@users.noreply.github.com>
2020-12-19 19:21:25 +0500

    Now Added function of Delete.You can now delete notes as well

--- Sticky Notes App/Js/app.js
+++ Sticky Notes App/Js/app.js
@@ -1,4 +1,6 @@
 console.log('Welcome to Notes App. this is app.js');
+showNotes();
+
 
 //If User Adds a note, Add it to local Storage
 let addBtn = document.getElementById('addBtn');
@@ -15,5 +17,51 @@ addBtn.addEventListener("click", function (e) {
     localStorage.setItem("notes", JSON.stringify(notesObject));
     addTxt.value = "";
     console.log(notesObject);
-    // showNotes();
-}) 
+    showNotes();
+})
+
+//Function to show Elements from localStorage
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObject = [];
+    }
+    else {
+        notesObject = JSON.parse(notes);
+    }
+    let html = "";
+    notesObject.forEach(function (element, index) {
+        html += `
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Note ${index + 1}</h5>
+                <p class="card-text">${element + 1}</p>
+                <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+            </div>
+        </div>`;
+    });
+    let notesElm = document.getElementById('notes');
+    if (notesObject.length != 0) {
+        notesElm.innerHTML = html;
+    }
+    else {
+        notesElm.innerHTML = `Nothing to show!"Add a Note"`;
+    }
+}
+
+//Function to Delete Notes
+function deleteNote(index) {
+    console.log('I am deleting', index);
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObject = [];
+    }
+    else {
+        notesObject = JSON.parse(notes);
+    }
+
+    notesObject.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObject));
+    showNotes();
+}
