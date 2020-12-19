b32d836b8ef1eaf3930c53c3036844d8e31f9203
talha faisal <57016608+talha442@users.noreply.github.com>
2020-12-19 18:38:15 +0500

    Added Some Changes On Text Area & worked on application

--- Sticky Notes App/Js/app.js
+++ Sticky Notes App/Js/app.js
@@ -1,1 +1,19 @@
 console.log('Welcome to Notes App. this is app.js');
+
+//If User Adds a note, Add it to local Storage
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click", function (e) {
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObject = [];
+    }
+    else {
+        notesObject = JSON.parse(notes);
+    }
+    notesObject.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObject));
+    addTxt.value = "";
+    console.log(notesObject);
+    // showNotes();
+}) 
